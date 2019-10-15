6b13dc974304b06ffbdbafffe98f01577486bdf2
Abby Gottlich <abby.gottlich@gmail.com>
2019-10-15 11:08:40 -0500

    basic redux working

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React, { Component } from "react";
 import HelloWorld from "./HelloWorld";
+import reducer from "./reducers/index";
+import { createStore } from "redux";  
+
+const state = {
+  tech: "React"
+};
+const store = createStore(reducer, state);
 
 class App extends Component {
-  state = {
-    tech: "React"
-  };
   render() {
-    return <HelloWorld tech={this.state.tech} />;
+    return <HelloWorld tech={store.getState().tech} />;
   }
 }
 

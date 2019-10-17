43ba3b7f8be3c7139e786e99f09f5b9a756b6034
Abby Gottlich <abby.gottlich@gmail.com>
2019-10-17 11:28:31 -0500

    subscribed to store - text rendering on button click

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,11 @@
 export default (state, action) => {
     switch (action.type) {
       case "SET_TECHNOLOGY":
-        console.log(action);
+        return {
+          ...state,
+          tech: action.text
+        };
+  
       default:
         return state;
     }
